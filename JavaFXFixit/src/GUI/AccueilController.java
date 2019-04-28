fd8732d48a16c4512ce9c09f2e75927aa4fa3e8a
hphqlim <hphqlim@DESKTOP-5BM7VAF>
2019-04-28 20:22:38 +0100

    addgooglemaps

--- JavaFXFixit/src/GUI/AccueilController.java
+++ JavaFXFixit/src/GUI/AccueilController.java
@@ -5,28 +5,107 @@
  */
 package GUI;
 
+import com.lynden.gmapsfx.MapComponentInitializedListener;
+import com.lynden.gmapsfx.javascript.object.*;
+import com.sun.prism.PhongMaterial;
+import javafx.fxml.Initializable;
+import com.lynden.gmapsfx.GoogleMapView;
+import com.lynden.gmapsfx.MapComponentInitializedListener;
+import com.lynden.gmapsfx.javascript.event.MouseEventHandler;
+import com.lynden.gmapsfx.javascript.event.UIEventType;
+
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
-
-/**
- * FXML Controller class
- *
- * @author hphqlim
- */
-public class AccueilController implements Initializable {
+public class AccueilController implements Initializable, MapComponentInitializedListener {
     @FXML
     private Button button;
-    /**
-     * Initializes the controller class.
-     * @param url
-     * @param rb
-     */
+
+    @FXML
+    private GoogleMapView mapView;
+
+    private GoogleMap map;
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
-    
+        mapView.setKey("AIzaSyCpBZ4AjkZIoLWHnYYF5qsdQO5CTnCpcko");
+        mapView.addMapInializedListener(this);
+
+    }
+
+    @Override
+    public void mapInitialized() {
+        LatLong joeSmithLocation = new LatLong(47.6197, -122.3231);
+        LatLong joshAndersonLocation = new LatLong(47.6297, -122.3431);
+        LatLong bobUnderwoodLocation = new LatLong(47.6397, -122.3031);
+        LatLong tomChoiceLocation = new LatLong(47.6497, -122.3325);
+        LatLong fredWilkieLocation = new LatLong(47.6597, -122.3357);
+
+
+        //Set the initial properties of the map.
+        MapOptions mapOptions = new MapOptions();
+
+        mapOptions.center(new LatLong(47.6097, -122.3331))
+                .mapType(MapTypeIdEnum.HYBRID)
+                .overviewMapControl(false)
+                .panControl(false)
+                .rotateControl(false)
+                .scaleControl(false)
+                .streetViewControl(false)
+                .zoomControl(true)
+                .zoom(12);
+
+        map = mapView.createMap(mapOptions);
+
+        //Add markers to the map
+        MarkerOptions markerOptions1 = new MarkerOptions();
+        markerOptions1.position(joeSmithLocation);
+
+        MarkerOptions markerOptions2 = new MarkerOptions();
+        markerOptions2.position(joshAndersonLocation);
+        markerOptions2.title("gfhdgfg");
+        markerOptions2.visible(true);
+
+
+        MarkerOptions markerOptions3 = new MarkerOptions();
+        markerOptions3.position(bobUnderwoodLocation);
+
+        MarkerOptions markerOptions4 = new MarkerOptions();
+        markerOptions4.position(tomChoiceLocation);
+
+        MarkerOptions markerOptions5 = new MarkerOptions();
+        markerOptions5.position(fredWilkieLocation);
+        
+        Marker joeSmithMarker = new Marker(markerOptions1);
+        Marker joshAndersonMarker = new Marker(markerOptions2);
+        Marker bobUnderwoodMarker = new Marker(markerOptions3);
+        Marker tomChoiceMarker= new Marker(markerOptions4);
+        Marker fredWilkieMarker = new Marker(markerOptions5);
+        
+        map.addMarker( joeSmithMarker );
+        map.addMarker( joshAndersonMarker );
+        map.addMarker( bobUnderwoodMarker );
+        map.addMarker( tomChoiceMarker );
+        map.addMarker( fredWilkieMarker );
+        
+        InfoWindowOptions infoWindowOptions = new InfoWindowOptions();
+        infoWindowOptions.content("<img src=\"IMG.jpg\" alt=Smiley face height=42 width=42>"
+                + "Current Location: Safeway<br>"
+                + "ETA: 45 minutes" );
+
+        InfoWindow fredWilkeInfoWindow = new InfoWindow(infoWindowOptions);
+        fredWilkeInfoWindow.open(map, fredWilkieMarker);
+        fredWilkeInfoWindow.open(map, bobUnderwoodMarker);
+        MarkerOptions markerOptions = new MarkerOptions();
+
+        markerOptions.position( new LatLong(47.6, -122.3) )
+                .visible(Boolean.TRUE)
+                .title("My Marker");
+
+        Marker marker = new Marker( markerOptions );
+        map.addMarker(marker);
+      
+    }
 }
