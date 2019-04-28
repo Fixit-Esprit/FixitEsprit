f83677727dff40f992d58cb7bdb74c04107b769c
hphqlim <hphqlim@DESKTOP-5BM7VAF>
2019-04-28 01:33:32 +0100

    commitapp

---
+++ JavaFXFixit/src/utilis/DaoConnection.java
@@ -0,0 +1,53 @@
+package utilis;
+
+import static java.lang.System.out;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author hp halim
+ */
+public class DaoConnection {
+
+    private static Connection connect;
+    private static DaoConnection daos;
+    private final PreparedStatement pst = null;
+    private final String url = "jdbc:mysql://localhost:3306/Fixit";
+    private final String user = "root";
+    private final String pass = "";
+    ResultSet resultat = null;
+
+    private DaoConnection() {
+        try {
+            connect = DriverManager.getConnection(url, user, pass);
+        } catch (SQLException ex) {
+            System.out.print(ex.getMessage());
+        }
+    }
+
+    public static DaoConnection getInstance() {
+        if (daos == null) {
+            daos = new DaoConnection();
+        }
+        return daos;
+    }
+
+    public static Connection getConnect() {
+        return connect;
+    }
+
+}
