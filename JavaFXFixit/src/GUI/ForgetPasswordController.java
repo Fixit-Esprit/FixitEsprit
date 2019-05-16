92e87cef7d11a490c8d9dec1629afa64c76b2163
marwa <m.saieb@cmrp.fr>
2019-05-16 15:01:17 +0100

    Ajout de compte prestataire et la synchronisation avec l'acceuil

--- JavaFXFixit/src/GUI/ForgetPasswordController.java
+++ JavaFXFixit/src/GUI/ForgetPasswordController.java
@@ -5,9 +5,17 @@
  */
 package GUI;
 
+ 
 import java.net.URL;
+import java.util.Properties;
 import java.util.ResourceBundle;
 import javafx.fxml.Initializable;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 
 /**
  * FXML Controller class
@@ -22,6 +30,37 @@ public class ForgetPasswordController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO 
+        
+        
+        
     }    
+  public void sendMail(String userMail,String pass,String dest,String sujet,String contenu) throws MessagingException{
     
+        
+        String to = dest;
+        String host = "smtp.gmail.com";
+        Properties prop = System.getProperties();
+        prop.put("mail.smtp.starttls.enable", "true");
+        prop.put( "mail.smtp.host", host );
+        prop.put("mail.smtp.user", userMail);
+        prop.put("mail.smtp.password", pass);
+        prop.put("mail.smtp.port", "587");
+        prop.put("mail.smtp.auth", "true");
+        prop.put("mail.smtp.ssl.trust", "smtp.gmail.com");
+        
+        Session session = Session.getDefaultInstance(prop);
+        MimeMessage msg = new MimeMessage(session);
+        msg.setFrom(new InternetAddress(userMail));
+         
+        InternetAddress toAdresse = new InternetAddress(to);
+                
+        msg.setRecipient(Message.RecipientType.TO, toAdresse);
+        msg.setSubject(sujet);
+        msg.setContent(contenu,"text/html; charset=utf-8");
+        Transport transport = session.getTransport("smtp");
+        transport.connect(host, userMail, pass);
+        transport.sendMessage(msg, msg.getAllRecipients());
+        transport.close();
+    
+    }  
 }
